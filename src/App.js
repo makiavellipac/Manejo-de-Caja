d24620b11d26d87a98d0422bdafa8114f3adb77e
MakiavelliPac <ridesburak@gmail.com>
2020-05-16 14:49:28 -0500

    Se creo logica para conectar a CloseBox con OpenBox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OpenBox from './componentes/OpenBox';
 import CloseBox from './componentes/CloseBox';
 import './app.css';
 function App() {
+  const [active,setActive]=useState(false);
+
+  const clickedActive=()=>setActive(true);
   return (
+    
     <div className="app_page">
-      <OpenBox/>
-      <CloseBox/>
+      <OpenBox cliked={clickedActive}/>
+      <CloseBox active={active}/>
     </div>
   );
 }
