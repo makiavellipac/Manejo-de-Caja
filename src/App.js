e60cb17979521d1de9c1eff8468ce5f70b8629d8
MakiavelliPac <ridesburak@gmail.com>
2020-05-16 12:21:29 -0500

    Se Monto el componente CloseBox en el App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import OpenBox from './componentes/OpenBox'
-
-
+import OpenBox from './componentes/OpenBox';
+import CloseBox from './componentes/CloseBox';
+import './app.css';
 function App() {
   return (
-    <>
+    <div className="app_page">
       <OpenBox/>
-    </>
+      <CloseBox/>
+    </div>
   );
 }
 
