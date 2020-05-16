7b11758af8b242c39055428ddfdbe8935b9bafdd
MakiavelliPac <ridesburak@gmail.com>
2020-05-16 16:55:41 -0500

    Se configuro prop-types en el componnte InputWithLabel

--- src/componentes/InputWithLabel/index.js
+++ src/componentes/InputWithLabel/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import './index.css'
+import propTypes from 'prop-types';
+import './index.css';
 
-const InputWithLabel=({title,children,w="auto",h="auto"})=>{
+const InputWithLabel=({title,children,w,h})=>{
     return(
             <label style={{width:w,height:h}} className="inputWithLabel">
                 {title}
                 {children}
             </label>
     )
+}
+
+InputWithLabel.defaultProps={
+    title:'',
+    children:<p></p>,
+    w:"auto",
+    h:"auto"
+}
+
+InputWithLabel.propTypes={
+    title:propTypes.string,
+    children:propTypes.element,
+    w:propTypes.string,
+    h:propTypes.string
 }
 
 export default InputWithLabel;
